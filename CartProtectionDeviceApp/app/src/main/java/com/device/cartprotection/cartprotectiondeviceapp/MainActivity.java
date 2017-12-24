c7d021db268466a96ed12d3e600d0df34ea980a6
Laptop\Hundia <eli.hundia@gmail.com>
2017-12-24 21:11:21 +0200

    1. Cart protector app with basic location service 2. some httpserver java example - will probebly be replaced by either jersey or spring web service

---
+++ CartProtectionDeviceApp/app/src/main/java/com/device/cartprotection/cartprotectiondeviceapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.device.cartprotection.cartprotectiondeviceapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView id_tv = (TextView) findViewById(R.id.device_id);
+        id_tv.setText("1");
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //  Check that we have the required permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d("error","No permission..!");
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Log.d("Location Service", "Lat: " + location.getLatitude() + " Long: " + location.getLongitude());
+                            ((TextView) findViewById(R.id.lon_val)).setText(Double.toString(location.getLongitude()));
+                            ((TextView) findViewById(R.id.lat_val)).setText(Double.toString(location.getLatitude()));
+                        }
+                    }
+                });
+    }
+}
